181be6d3e6cb28fde9f4339d4a0012ce72e54c72
Khanyisile Mathe <khanyisile.mathe@chillisoft.co.za>
2021-05-21 16:57:32 +0200

    test

--- src/tests-to-implement/06_PubSub.spec.ts
+++ src/tests-to-implement/06_PubSub.spec.ts
@@ -6,19 +6,37 @@ describe('PubSub', () => {
     it.skip('calls subscription callback when publish occurs on channel', async () => {
       // Arrange
       const sut = PubSub.getInstance();
-      const callBack = jest.fn();
+      let myPromiseResolve: Function;
+      const  myPromise = new Promise((resolve, reject)=>{ myPromiseResolve = resolve});
+      const callBack = jest.fn().mockImplementation(()=> myPromiseResolve());
       const payload ="message";
       // Act
       sut.subscribe("Channel1", callBack);
-      sut.publish("Channel1",payload );
+      sut.publish("Channel1", payload );
       // Assert
+      await myPromise;
       expect(callBack).toHaveBeenCalledWith(payload);
     })
 
-    it.skip('calls all subscription callbacks when publish occurs on channel', async () => {
+    it('calls all subscription callbacks when publish occurs on channel', async () => {
       // Arrange
+      const sut = PubSub.getInstance();
+      let myFirstPromiseResolve: Function;
+      let mySecondPromiseResolve: Function;
+      const  myFirstPromise = new Promise((resolve, reject)=>{ myFirstPromiseResolve = resolve});
+      const  mySecondPromise = new Promise((resolve, reject)=>{ mySecondPromiseResolve = resolve});
+      const firstCallBack = jest.fn().mockImplementation(()=> myFirstPromiseResolve());
+      const secondCallBack = jest.fn().mockImplementation(()=> mySecondPromiseResolve());
+      const payload ="message";
       // Act
+      sut.subscribe("Channel1", firstCallBack);
+      sut.subscribe("Channel1", secondCallBack);
+      sut.publish("Channel1", payload );
       // Assert
+      await myFirstPromise;
+      await mySecondPromise;
+      expect(firstCallBack).toHaveBeenCalledWith(payload);
+      expect(secondCallBack).toHaveBeenCalledWith(payload);
     })
   })
 })
--- src/tests-to-implement/07_use_it_all.spec.ts
+++ src/tests-to-implement/07_use_it_all.spec.ts
@@ -1,28 +1,93 @@
 import { describe, expect, it } from '@jest/globals'
+import { InMemoryCache } from '../dependencies/InMemoryCache'
+import { Item } from '../dependencies/Item'
+import { ItemRepository } from '../dependencies/ItemRepository'
 import { ItemProcessor } from "../tests-to-implement/07_use_it_all"
+import { PubSub, PubSubChannels } from './06_PubSub'
 
 describe('ItemProcessor', () => {
   describe('processItems', () => {
-    it.skip('will not process items if processing is already busy', async () => {
+    it('will not process items if processing is already busy', async () => {
       // Arrange
+      const cache = new InMemoryCache();
+      const itemRepository = new ItemRepository();
+      const sut = new ItemProcessor(cache , itemRepository)
+      const items : Item[] = [
+        {
+          id: "1",
+          name: "Bob",
+          price: 20,
+          description: "Food",
+          created: new Date
+        },
+        {
+          id: "2",
+          name: "Bob",
+          price: 5,
+          description: "Juice",
+          created: new Date
+        }
+      ];
+      
+      jest.spyOn(itemRepository, "getAll").mockResolvedValue(items);
+      sut["isProcessing"] = true;
+      var processedItems = sut["processedItems"];
       // Act
+      await sut.processItems();
       // Assert
+      expect(processedItems.length).toBe(0);
     })
 
     describe('given single unprocessed item', () => {
-      it.skip('updates the cache with the item', async () => {
+      it('updates the cache with the item', async () => {
         // Arrange
+        const cache = new InMemoryCache();
+        const itemRepository = new ItemRepository();
+        const sut = new ItemProcessor(cache , itemRepository)
+        const cachedItems : Item[] = [];
+        const items : Item[] = [
+          {
+            id: "1",
+            name: "Bob",
+            price: 20,
+            description: "Food",
+            created: new Date
+          }
+        ];
+        
+        jest.spyOn(itemRepository, "getAll").mockResolvedValue(items);
+        jest.spyOn(cache, "update").mockImplementation(item => cachedItems.push(item));
         // Act
+        await sut.processItems();
         // Assert
+        expect(cachedItems.length).toBe(1);
       })
 
-      it.skip('publishes an item updated message', async () => {
+      it('publishes an item updated message', async () => {
         // Arrange
+        const pubSub = PubSub.getInstance()
+        const cache = new InMemoryCache();
+        const itemRepository = new ItemRepository();
+        const sut = new ItemProcessor(cache , itemRepository)
+        const items : Item[] = [
+          {
+            id: "1",
+            name: "Bob",
+            price: 20,
+            description: "Food",
+            created: new Date
+          }
+        ];
+        
+        jest.spyOn(itemRepository, "getAll").mockResolvedValue(items);
+        const actual = jest.spyOn(pubSub, "publish");
         // Act
+        await sut.processItems();
         // Assert
+        expect(actual).toHaveBeenCalledWith("item:updated", items[0]);
       })
 
-      it.skip('does not process items that have already been processed', async () => {
+      it('does not process items that have already been processed', async () => {
         // Arrange
         // Act
         // Assert
